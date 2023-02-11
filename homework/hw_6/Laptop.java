973fa8405af1f0db9b4a2afa449f38b5675f6691
VadimR <rakhmaninvs@gmail.com>
2023-02-11 15:18:17 +0300

    homework 6

--- homework/hw_6/Laptop.java
+++ homework/hw_6/Laptop.java
@@ -1,29 +1,42 @@
 package hw_6;
 
 public class Laptop {
+    private Integer id;
     private String cpu; 
     private Integer ram;
     private Integer storage;
     private String os;
     private Boolean gpu;
 
-    public Laptop(String cpu, int ram, int storage, String os, boolean gpu) {
+    private static Integer incrementalId = 0;
+
+    public Laptop(String cpu, Integer ram, Integer storage, String os, Boolean gpu) {
+        this.id = setId();
         this.cpu = cpu;
         this.ram = ram;
         this.storage = storage;
         this.os = os;
         this.gpu = gpu;
     }
+    
+    public Integer getId() {
+        return id;
+    }
+    
+    private Integer setId() {
+        incrementalId++;
+        return incrementalId;
+    }
 
     public String getCpu() {
         return cpu;
     }
 
-    public int getRam() {
+    public Integer getRam() {
         return ram;
     }
 
-    public int getStorage() {
+    public Integer getStorage() {
         return storage;
     }
 
@@ -31,14 +44,14 @@ public class Laptop {
         return os;
     }
 
-    public boolean haveGpu() {
+    public Boolean haveGpu() {
         return gpu;
     }
 
 
     @Override
     public String toString() {
-        return "laptop [cpu=" + cpu + ", ram=" + ram + ", storage=" + storage + ", os=" + os + ", gpu=" + gpu + "]";
+        return "laptop " + id + "-> cpu=" + cpu + "\tram=" + ram + "\tstorage=" + storage + "\tos=" + os + "\tgpu=" + gpu;
     }
     
     
--- homework/hw_6/task.java
+++ homework/hw_6/task.java
@@ -1,86 +1,141 @@
 package hw_6;
 
-
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
+import java.util.stream.Collectors;
 
-/*Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
-Создать множество ноутбуков.
-Написать метод, который будет запрашивать у пользователя критерий фильтрации и выведет ноутбуки, отвечающие фильтру. Критерии фильтрации можно хранить в Map.
-
-Например: “Введите цифру, соответствующую необходимому критерию:
-1 - ОЗУ
-2 - Объем ЖД
-3 - Операционная система
-4 - Цвет
-
-Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно также в Map.
-
-Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
-
-Класс сделать в отдельном файле*/
 public class task {
-    public static void main(String[] args) {
-        Set<Laptop> laptops = fillLaptopSet();
+    /*без этого не работает фильтр. ругается на map(стр. 81, 83) и paramChoice(стр. 81, 83, 91, 93)
+    ошибка Local variable map defined in an enclosing scope must be final or effectively final Java(536871575) */
+    static Map<Integer, String> map = new HashMap<>();
+    static volatile Integer paramChoise = 0;
+    static Scanner sc = new Scanner(System.in);
+    
+    public static void main(String[] args) throws Exception {
+
+        ArrayList<Laptop> laptops = fillLaptopArray();
+        Boolean runFlag = true;
+        Integer choise;
         
-        
+        while (runFlag) {
+            System.out.println("\n1 - Вывод ноутбуков на экран");
+            System.out.println("2 - Отфильтровать ноутбуки");
+            System.out.println("3 - Сбросить фильтр");
+            System.out.println("4 - Выход");
+            System.out.print("\nВыберете действие: ");
+            switch (sc.nextInt()) {
+                case 1:
+                    if (!laptops.isEmpty()) { 
+                        for (Laptop laptop : laptops) {
+                            System.out.println(laptop);
+                        }
+                    }
+                    else System.out.println("Нет подходящих ноутбуков");
+                    break;
+                case 2:
+                    choise = chooseCategory();
+                    laptops = filterLaptops(laptops, choise);
+                    break;
+                case 3:
+                    laptops = fillLaptopArray();
+                    break;
+                case 4:
+                    runFlag = false;
+                    sc.close();
+                    break;
+                default:
+                    System.out.println("неправильный номер\n");
+                    break;
+            }
+        }
     }
 
-
-
     public static Integer chooseCategory() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("1 - Производитель ЦП");
+        
+        System.out.println("1 - Процессор");
         System.out.println("2 - Оперативная память");
         System.out.println("3 - Емкость хранилища данных");
         System.out.println("4 - Операционная система");
         System.out.println("5 - Наличие видеокарты");
+        
         while (true){
             System.out.print("Введите номер категории для фильтра: ");
             Integer choise = sc.nextInt();
-            if (choise > 0 && choise < 6){
-                sc.close();
-                return choise;
-            }
-            System.out.println("неправильный номер");
+            if (choise > 0 && choise < 6) return choise;
+            System.out.println("неправильный номер\n");
         }
     }
 
-    public static void name(int num, Set<Laptop> laptops) {
-        Scanner sc = new Scanner(System.in);
-        Set<Object> categorySet = new HashSet<>();
-        for (Laptop item : laptops) {
-            switch (num) {
-                case 1:
-                    categorySet.add(item.getCpu());
-                    break;
-                case 2:
-                    categorySet.add(item.getRam());
-                    break;
-                case 3:
-                    categorySet.add(item.getStorage());
-                    break;
-                case 4:
-                    categorySet.add(item.getOs());
-                    break;
-                case 5:
-                    categorySet.add(item.haveGpu());
-                    break;
-            }
-        }        
+    public static ArrayList<Laptop> filterLaptops(ArrayList<Laptop> laptops, Integer choise) {
+        
+        Boolean filterBool;
+        switch (choise) {
+            case 1: case 4: // процессор, ос
+                getCategorySet(map, laptops, choise);
+                map.forEach((key, value) -> System.out.println(key + " - " + value));
+                System.out.print("Введите номер  для фильтра: ");
+                paramChoise = sc.nextInt();
+
+                if (choise.equals(1)) laptops = laptops.stream().filter(l -> l.getCpu().equals(map.get(paramChoise)))
+                                                            .collect(Collectors.toCollection(ArrayList::new));
+                else laptops = laptops.stream().filter(l -> l.getOs().equals(map.get(paramChoise)))
+                                        .collect(Collectors.toCollection(ArrayList::new));
+                break;
+                
+            case 2: case 3: // озу и диск
+                System.out.print("Введите минимальное значение: ");
+                paramChoise = sc.nextInt();
+
+                if(choise.equals(2)) laptops = laptops.stream().filter(l -> l.getRam() >= paramChoise)
+                                                            .collect(Collectors.toCollection(ArrayList::new));
+                else laptops = laptops.stream().filter(l -> l.getStorage() >= paramChoise)
+                                        .collect(Collectors.toCollection(ArrayList::new));
+                
+                break;
+            case 5: // видеокарта
+                System.out.print("1 - есть, 0 - нет: ");
+                paramChoise = sc.nextInt();
+                if (paramChoise == 1) filterBool = true;
+                else filterBool = false;
+
+                if (choise.equals(5)) laptops = laptops.stream().filter(l -> l.haveGpu().equals(filterBool))
+                                                            .collect(Collectors.toCollection(ArrayList::new));
+                break;
+        } 
+        return laptops;     
     }
 
-    public static Set<Laptop> fillLaptopSet() {
-        Set<Laptop> laptops = new HashSet<>();
-        laptops.add(new Laptop("AMD", 16, 512, "Linux", true));
-        laptops.add(new Laptop("Intel", 32, 1024, "Windows", true));
-        laptops.add(new Laptop("Intel", 4, 256, "Windows", false));
-        laptops.add(new Laptop("AMD", 8, 400, "Linux", false));
-        laptops.add(new Laptop("Intel", 16, 512, "Windows", false));
-        laptops.add(new Laptop("Intel", 8, 400, "Windows", false));
-        return laptops;        
+    // наполняет словарь map всеми уникальными значениями для выбранной характеристики
+    public static void getCategorySet(Map<Integer, String> map, ArrayList<Laptop> laptops, Integer choise) {
+        map.clear();
+        int keyCounter = 1;
+        String value = "";
+        
+        for (int i = 0; i < laptops.size(); i++) {
+            if (choise.equals(1)) value = laptops.get(i).getCpu();
+            else value = laptops.get(i).getOs();
+            
+            if (!map.containsValue(value)){
+                map.put(keyCounter, value);
+                keyCounter++;
+            }
+        }
+    }
+
+    public static ArrayList<Laptop> fillLaptopArray() {
+
+        ArrayList<Laptop> laptops = new ArrayList<>();
+        laptops.add(new Laptop("AMD Athlon", 16, 512, "Linux", true));
+        laptops.add(new Laptop("Intel Core i7", 32, 1024, "Windows", true));
+        laptops.add(new Laptop("Intel Celeron", 4, 256, "Windows", false));
+        laptops.add(new Laptop("AMD Ryzen 3", 8, 400, "Linux", false));
+        laptops.add(new Laptop("Intel Core i7", 16, 512, "Windows", false));
+        laptops.add(new Laptop("Intel Core i3", 8, 400, "Windows", false));
+        laptops.add(new Laptop("AMD Ryzen 3", 32, 1024, "Linux", true));
+        laptops.add(new Laptop("Intel Core i7", 16, 1024, "Windows", true));
+        laptops.add(new Laptop("Intel Core i3", 8, 512, "macOS", true));
+        return laptops;
     }
 }
